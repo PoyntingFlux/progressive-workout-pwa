67836c88b60ac2592287f67b65087d8667ddec19
PoyntingFlux <way.paul@gmail.com>
2025-12-21 13:09:40 -0600

    V13

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "progressive-workout-cache-v12";
+const CACHE_NAME = "progressive-workout-cache-v13";
 const ASSETS = [
   "./",
-  "./index.html?v=12",
-  "./styles.css?v=12",
-  "./app.js?v=12",
+  "./index.html?v=13",
+  "./styles.css?v=13",
+  "./app.js?v=13",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
